0e4533edbc1d6e080695bb6002e8431c111fbd64
victor <vjbsh@hotmail.com>
2023-05-14 21:19:47 -0600

    login redirect successful but now the css is broken for the buttons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Login from "./components/login";
+import ProductList from "./components/productList";
 
 function App() {
-  return (
-    <div className="App">
-      <h1 className="title">PRODUCTOS DISPONIBLES</h1>
-    </div>
-  );
+    return (
+            <div className="App" >
+                <h1 className="title">
+                    PRODUCTOS DISPONIBLES
+                </h1>
+                <Login />
+            </div>
+    );
 }
 
 export default App;
--- src/components/login.js
+++ src/components/login.js
@@ -1,67 +1,57 @@
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { Button, Container, Form, Row } from "react-bootstrap";
-import { render } from "@testing-library/react";
-import ProductList from "./productList";
+import { useState } from "react";
+import {Button, Container, Form, Row} from "react-bootstrap";
+import {useNavigate} from "react-router-dom";
+import './style.css';
 
-const Login = () => {
-  const navigate = useNavigate();
-  const [showForm, setShowForm] = useState(false);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+const Login = ()=> {
+    const navigate = useNavigate();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-  const loginHandler = (e) => {
-    try {
-      e.preventDefault();
-      console.log(">>Login: " + email + " p = " + password);
-      fetch(`https://api-rest-batarseshija.azurewebsites.net/login`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email: email, password: password }),
-      })
-        .then((response) => response.json())
-        .then((dataResponse) => {})
-        .then(() => {
-          navigate("/productList");
-        });
-    } catch (err) {
-      console.log(err);
+    const loginHandler = (event) => {
+        event.preventDefault();
+        try {
+            const requestBody = {email: email, password: password}
+            fetch(`https://api-rest-batarseshija.azurewebsites.net/login`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(requestBody)
+            })
+                .then(response=>{
+                    if (!response.ok){
+                        throw new Error(response.statusMessage || "Backend error");
+                    }
+                }).then(() => {
+                    navigate("/productList", {replace: true});
+            });
+        } catch (err) {
+            console.log(err);
+        }
     }
-  };
 
-  return (
-    <Container named="container">
-      <Row>
-        <Form onSubmit={loginHandler}>
-          <Form.Group className="mb-3" controlId="email">
-            <Form.Label className="mb2"> EMAIL: </Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Escribe tu email"
-              value={email}
-              onChange={(txt) => setEmail(txt.target.value)}
-            />
-          </Form.Group>
+    return (
+        <Container named="container">
+            <Row>
+                <Form onSubmit={loginHandler}>
+                    <Form.Group className="mb-3" controlId="email">
+                        <Form.Label className="mb2"> EMAIL: </Form.Label>
+                        <Form.Control type="text" placeholder="Escribe tu email" value={email} onChange={(txt) => setEmail(txt.target.value)} />
+                    </Form.Group>
 
-          <Form.Group className="mb-3" controlId="password">
-            <Form.Label className="mb2">Password:</Form.Label>
-            <Form.Control
-              type="password"
-              placeholder="Escribe tu contraseña"
-              value={password}
-              onChange={(txt) => setPassword(txt.target.value)}
-            />
-          </Form.Group>
+                    <Form.Group className="mb-3" controlId="password">
+                        <Form.Label className="mb2">Password:</Form.Label>
+                        <Form.Control type="password" placeholder="Escribe tu contraseña" value={password} onChange={(txt) => setPassword(txt.target.value)} />
+                    </Form.Group>
 
-          <Button variant="primary" type="submit">
-            Iniciar Sesion
-          </Button>
-        </Form>
-      </Row>
-    </Container>
-  );
-};
+                    <Button variant="primary" type="submit">
+                        Iniciar Sesion
+                    </Button>
+                </Form>
+            </Row>
+        </Container>
+    )
+}
 
 export default Login;
