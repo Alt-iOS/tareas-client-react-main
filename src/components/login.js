2b3cdf5464274db14ad0ef079d19f1fa3b1081ae
victor <vjbsh@hotmail.com>
2024-04-21 18:20:21 -0600

    moved backend to new deployment

--- src/components/login.js
+++ src/components/login.js
@@ -1,93 +1,97 @@
 import { useState } from "react";
-import {Button, Container, Form, Row} from "react-bootstrap";
-import {useNavigate} from "react-router-dom";
-import './style.css';
+import { Button, Container, Form, Row } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import "./style.css";
 import UserForm from "./userForm";
 
-const Login = ()=> {
-    const navigate = useNavigate();
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [errorMessage, setErrorMessage] = useState('');
-    const [showForm, setShowForm] = useState(false);
+const Login = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [showForm, setShowForm] = useState(false);
 
-
-    const loginHandler = (event) => {
-        event.preventDefault();
-        try {
-            const requestBody = {email: email, password: password}
-            //fetch(`https://api-rest-batarseshija.azurewebsites.net/login`, {
-            fetch('http://localhost:8080/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(requestBody)
-            })
-                .then(response => {
-                    if (!response.ok) {
-                    alert("Usuario o contraseña incorrectos");
-                    } else {
-
-                        navigate("/productList", {replace: true});
-                    }
-                });
-        } catch (err) {
-            console.log(err);
+  const loginHandler = (event) => {
+    event.preventDefault();
+    try {
+      const requestBody = { email: email, password: password };
+      //fetch(`https://api-rest-batarseshija.azurewebsites.net/login`, {
+      fetch(`https://stingray-app-n6ohc.ondigitalocean.app/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(requestBody),
+      }).then((response) => {
+        if (!response.ok) {
+          alert("Usuario o contraseña incorrectos");
+        } else {
+          navigate("/productList", { replace: true });
         }
+      });
+    } catch (err) {
+      console.log(err);
     }
+  };
 
-    const createHandler = (event) => {
-        try {
-                //fetch("https://api-rest-batarseshija.azurewebsites.net/register", {
-                fetch('http://localhost:8080/register', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(event),
-            })
-                .then(response => {
-                    if (response.status === 409) {
-                        alert("El usuario ya existe");
-                    }
-                    else if (!response.ok) {
-                        alert("Error al crear el usuario");
-                    }
-                });
-        } catch (err) {
-            console.log(err);
+  const createHandler = (event) => {
+    try {
+      //fetch("https://api-rest-batarseshija.azurewebsites.net/register", {
+      fetch(`https://stingray-app-n6ohc.ondigitalocean.app/register`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(event),
+      }).then((response) => {
+        if (response.status === 409) {
+          alert("El usuario ya existe");
+        } else if (!response.ok) {
+          alert("Error al crear el usuario");
         }
+      });
+    } catch (err) {
+      console.log(err);
     }
+  };
 
+  return (
+    <Container named="container">
+      <Row>
+        <Form onSubmit={loginHandler}>
+          <Form.Group className="mb-3" controlId="email">
+            <Form.Label className="mb2"> EMAIL: </Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Escribe tu email"
+              value={email}
+              onChange={(txt) => setEmail(txt.target.value)}
+            />
+          </Form.Group>
 
-    return (
-        <Container named="container">
-            <Row>
-                <Form onSubmit={loginHandler}>
-                    <Form.Group className="mb-3" controlId="email">
-                        <Form.Label className="mb2"> EMAIL: </Form.Label>
-                        <Form.Control type="text" placeholder="Escribe tu email" value={email} onChange={(txt) => setEmail(txt.target.value)} />
-                    </Form.Group>
+          <Form.Group className="mb-3" controlId="password">
+            <Form.Label className="mb2">Password:</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Escribe tu contraseña"
+              value={password}
+              onChange={(txt) => setPassword(txt.target.value)}
+            />
+          </Form.Group>
 
-                    <Form.Group className="mb-3" controlId="password">
-                        <Form.Label className="mb2">Password:</Form.Label>
-                        <Form.Control type="password" placeholder="Escribe tu contraseña" value={password} onChange={(txt) => setPassword(txt.target.value)} />
-                    </Form.Group>
-
-                    <Button variant="primary" type="submit">
-                        Iniciar Sesion
-                    </Button>
-                </Form>
-                <Container className="boton">
-                    <Button  variant="primary" onClick={() => setShowForm(!showForm)}>
-                        {showForm ? "Cerrar" : "Registrarse"}
-                    </Button>
-                    {showForm && <UserForm onClickFn={createHandler}></UserForm>}
-                </Container>
-            </Row>
+          <Button variant="primary" type="submit">
+            Iniciar Sesion
+          </Button>
+        </Form>
+        <Container className="boton">
+          <Button variant="primary" onClick={() => setShowForm(!showForm)}>
+            {showForm ? "Cerrar" : "Registrarse"}
+          </Button>
+          {showForm && <UserForm onClickFn={createHandler}></UserForm>}
         </Container>
-    )
-}
+      </Row>
+    </Container>
+  );
+};
 
 export default Login;
--- src/components/productList.js
+++ src/components/productList.js
@@ -1,119 +1,119 @@
 //require("dotenv").config()
 import { useEffect, useState } from "react";
 import Producto from "./producto";
-import ProductForm from "./productForm"
-import { Button, Container } from 'react-bootstrap';
-import './style.css';
+import ProductForm from "./productForm";
+import { Button, Container } from "react-bootstrap";
+import "./style.css";
 
-
+const BACK_URL = "https://stingray-app-n6ohc.ondigitalocean.app/";
 
 const ProductList = () => {
+  const [producto, setProducto] = useState([]);
+  const [showForm, setShowForm] = useState(false);
 
+  // Hook para editar varibles de estado
+  useEffect(() => {
+    //fetch("https://api-rest-batarseshija.azurewebsites.net/products")
+    fetch(`https://stingray-app-n6ohc.ondigitalocean.app/products`)
+      .then((res) => res.json())
+      .then((data) => setProducto(data.data))
+      .catch((err) => console.log(`Error: ${err}`));
+  }, []);
 
-    const [producto, setProducto] = useState([]);
-    const [showForm, setShowForm] = useState(false);
+  const getProducts = () => {
+    // fetch("https://api-rest-batarseshija.azurewebsites.net/products")
+    fetch(`https://stingray-app-n6ohc.ondigitalocean.app/products`)
+      .then((res) => res.json())
+      .then((data) => setProducto(data.data))
+      .then((err) => console.log(`Error: ${err}`));
+  };
 
-    // Hook para editar varibles de estado
-    useEffect(() => {
-        //fetch("https://api-rest-batarseshija.azurewebsites.net/products")
-        fetch('http://localhost:8080/products')
-            .then((res) => res.json())
-            .then((data) => setProducto(data.data))
-            .catch((err) => console.log(`Error: ${err}`));
-    }, []);
-
-    const getProducts = () => {
-        // fetch("https://api-rest-batarseshija.azurewebsites.net/products")
-        fetch('http://localhost:8080/products')
-            .then((res) => res.json())
-            .then((data) => setProducto(data.data))
-            .then((err) => console.log(`Error: ${err}`));
+  const createProduct = (data) => {
+    try {
+      //fetch("https://api-rest-batarseshija.azurewebsites.net/products",
+      fetch(`https://stingray-app-n6ohc.ondigitalocean.app/products`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      })
+        .then((response) => response.json())
+        .then((dataResponse) => {
+          setProducto([...producto, dataResponse.data]);
+          setShowForm(false);
+        });
+    } catch (err) {
+      console.log(err);
     }
+  };
 
-    const createProduct = (data) => {
-        try {
-            //fetch("https://api-rest-batarseshija.azurewebsites.net/products",
-            fetch('http://localhost:8080/products',{
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(data),
-            })
-                .then(response => response.json())
-                .then(dataResponse => {
-                    setProducto([...producto, dataResponse.data]);
-                    setShowForm(false);
-                });
-        } catch (err) {
-            console.log(err);
-        }
+  const deleteProduct = (data) => {
+    try {
+      //fetch(`https://api-rest-batarseshija.azurewebsites.net/products/${data}`, {
+      fetch(`https://stingray-app-n6ohc.ondigitalocean.app/products/${data}`, {
+        method: "DELETE",
+      })
+        .then((response) => response.json())
+        .then((dataResponse) => {
+          console.log(dataResponse);
+          // setTareas([...tareas, dataResponse.data])
+        })
+        .then(() => {
+          getProducts();
+        });
+    } catch (err) {
+      console.log(err);
     }
+  };
 
-    const deleteProduct = (data) => {
-        try {
-            //fetch(`https://api-rest-batarseshija.azurewebsites.net/products/${data}`, {
-            fetch(`http://localhost:8080/products/${data}`, {
-                method: "DELETE"
-            })
-                .then(response => response.json())
-                .then(dataResponse => {
-                    console.log(dataResponse)
-                    // setTareas([...tareas, dataResponse.data])
-                })
-                .then(() => {
-                    getProducts()
-                })
-        } catch (err) {
-            console.log(err)
-        }
+  const updateProduct = (data) => {
+    try {
+      //fetch(`https://api-rest-batarseshija.azurewebsites.net/products/${data._id}`, {
+      fetch(
+        `https://stingray-app-n6ohc.ondigitalocean.app/products/${data._id}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        },
+      )
+        .then((response) => response.json())
+        .then((dataResponse) => {
+          //setProducto(producto.map(producto => producto.id === dataResponse.data.id ? dataResponse.data : producto));
+          setShowForm(false);
+        })
+        .then(() => {
+          getProducts();
+        });
+    } catch (err) {
+      console.log(err);
     }
+  };
 
-    const updateProduct = (data) => {
-        try {
-            //fetch(`https://api-rest-batarseshija.azurewebsites.net/products/${data._id}`, {
-            fetch(`http://localhost:8080/products/${data._id}`, {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(data),
-            })
-                .then(response => response.json())
-                .then(dataResponse => {
-                    //setProducto(producto.map(producto => producto.id === dataResponse.data.id ? dataResponse.data : producto));
-                    setShowForm(false);
-                }).then(() => {
-                    getProducts()
-                });
-        } catch (err) {
-            console.log(err);
-        }
-    }
-
-    // Regreso dinamico de informacion
-    return (
-        <div>
-            <h1 className="title">
-                    LISTA DE PRODUCTOS
-            </h1>
-            {producto.map((producto, index) => (
-                <Producto
-                    key={index}
-                    index={index}
-                    producto={producto}
-                    onDelete={deleteProduct}
-                    onUpdate={updateProduct}
-                />
-            ))}
-            <Container className="boton">
-                <Button  variant="primary" onClick={() => setShowForm(!showForm)}>
-                    {showForm ? "Cerrar" : "Agregar producto"}
-                </Button>
-                {showForm && <ProductForm onClickFn={createProduct}></ProductForm>}
-            </Container>
-        </div>
-    )
-}
+  // Regreso dinamico de informacion
+  return (
+    <div>
+      <h1 className="title">LISTA DE PRODUCTOS</h1>
+      {producto.map((producto, index) => (
+        <Producto
+          key={index}
+          index={index}
+          producto={producto}
+          onDelete={deleteProduct}
+          onUpdate={updateProduct}
+        />
+      ))}
+      <Container className="boton">
+        <Button variant="primary" onClick={() => setShowForm(!showForm)}>
+          {showForm ? "Cerrar" : "Agregar producto"}
+        </Button>
+        {showForm && <ProductForm onClickFn={createProduct}></ProductForm>}
+      </Container>
+    </div>
+  );
+};
 
 export default ProductList;
